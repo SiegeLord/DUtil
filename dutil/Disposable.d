ed7584570b7fce3b65205686fb40db70b6b8471b
SiegeLord <slabode@aim.com>
2011-09-22 18:16:42 -0400

    Add dutil/Disposable.d

---
+++ dutil/Disposable.d
@@ -0,0 +1,71 @@
+/*
+Copyright (c) 2010-2011 Pavel Sountsov
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+   3. This notice may not be removed or altered from any source
+   distribution.
+*/
+
+version(DebugDisposable) import tango.stdc.stdio;
+
+/**
+ * A simple class that formalizes the non-managed resource management. The advantage of using this
+ * is that with version DebugDisposable defined, it will track whether all the resources were disposed of
+ */
+class CDisposable
+{
+	this()
+	{
+		version(DebugDisposable)
+		{
+			InstanceCounts[this.classinfo.name]++;
+		}
+		
+		IsDisposed = false;
+	}
+	
+	void Dispose()
+	{
+		version(DebugDisposable)
+		{
+			if(!IsDisposed)
+			{
+				InstanceCounts[this.classinfo.name]--;
+			}
+		}
+
+		IsDisposed = true;
+	}
+	
+protected:
+	bool IsDisposed = false;
+}
+
+version(DebugDisposable)
+{
+	size_t InstanceCounts[char[]];
+
+	static ~this()
+	{
+		printf("Disposable classes instance counts:\n");
+		foreach(name, num; InstanceCounts)
+		{
+			printf("%s: %d\n", (name ~ "\0").ptr, num);
+		}
+	}
+}
